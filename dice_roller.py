0bed876f581b694045f42bf67a4b2827252cb3ff
AarushKiller <67967009+AarushKiller@users.noreply.github.com>
2021-09-07 20:05:44 +0545

    Added random number generators and comments

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,11 @@
+#imports
+import random
+#variables
+roll = random.randint(1,6)
+#main function
 def main():
-  print('You rolled a die')
-
+  print(f'You rolled a {roll}')
+#script so it runs on startup
 if __name__== "__main__":
   main()
+
